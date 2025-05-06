0e4aa8c31e1f5591a8c40f71c177b7396832ecf7
Andrew Lam <AndrewLam489@Gmail.com>
2025-05-06 11:06:03 -0400

    Add pytest fixture - parameterizing tests with multiple datasets

--- tests/test_HomePage.py
+++ tests/test_HomePage.py
@@ -5,20 +5,34 @@ from selenium.webdriver.chrome.service import Service
 from pageObjects.HomePage import HomePage
 from utilities.BaseClass import BaseClass
 
+import pytest
+
 
 class TestHomePage(BaseClass):
 
-    def test_formSubmission(self):
+    def test_formSubmission(self, getData):
 
         homePage = HomePage(self.driver)
-        homePage.getName().send_keys("Rahul Shetty")
-        homePage.getEmail().send_keys("Shetty")
-        homePage.getCheckbox().click()
-        homePage.getGender().click()
-        self.selectOptionByText(homePage.getGender(), "Male")
+        homePage.getName().send_keys(getData["firstName"])
+        homePage.getEmail().send_keys(getData["lastName"])
+        self.selectOptionByText(homePage.getGender(), getData["gender"])
 
         self.driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
         alertText = self.driver.find_element(By.CSS_SELECTOR, "[class*='alert-success']").text
 
         assert "Success" in alertText
+        self.driver.refresh()
+
+    @pytest.fixture(params=[
+        {"firstName": "Rahul", "lastName": "Shetty", "gender": "Male"},
+        {"firstName": "Andrew", "lastName": "Lam", "gender": "Male"}
+    ])
+    def getData(self, request):
+        """
+        Fixture to provide test data for form submission.
+
+        Returns:
+            dict: A dictionary containing test data for the form.
+        """
+        return request.param
